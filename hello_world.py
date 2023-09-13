f67d53d3629e29114166f99ceaa57e4b723b98a0
seungjae915 <77104943+seungjae915@users.noreply.github.com>
2023-09-13 17:24:53 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def print_hello():
-    print("Hello World!!")
-
-if __name__ == '__main__':
-    print_hello()
-    
+print("Hello World!!")
+def print_KIT():
+print("Kumoh National
+Institute of Technology.")
+if __name__ == '__main__’:
+print_KIT()
+print_hello()
